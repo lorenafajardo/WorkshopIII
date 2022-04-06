fa1b851b2d3f4e67c9c3295bc49b6b991cd7ff25
Daniela97B <daniigm97@gmail.com>
2022-04-05 22:24:47 -0500

    Getandset

--- objectsworkshop/Fruit.java
+++ objectsworkshop/Fruit.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * La clase KindFruit tiene definidos tres atributos, uno de ellos es un arreglo para definir los colores
  * a su vez hereda de la clase KindFruit que define a que clase de frutas pertenece.
- * @author Daniela Montano
+ * @author Daniela Montano Gomez
  *
  */
 public class Fruit extends KindFruit {
@@ -13,6 +13,25 @@ public class Fruit extends KindFruit {
 	public String name;
 	private float averageWeight;
 	public ArrayList<String> colors;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getAverageWeight() {
+		return averageWeight;
+	}
+	public void setAverageWeight(float averageWeight) {
+		this.averageWeight = averageWeight;
+	}
+	public ArrayList<String> getColors() {
+		return colors;
+	}
+	public void setColors(ArrayList<String> colors) {
+		this.colors = colors;
+	}
 	
 	
 

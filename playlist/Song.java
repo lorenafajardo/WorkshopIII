540cf53726240a364747d5bcd2cd183b48d43615
Ivonne Daniela Montano Gomez <94015994+Daniela97B@users.noreply.github.com>
2022-04-15 16:43:11 -0500

    Update Song.java

--- playlist/Song.java
+++ playlist/Song.java
@@ -1,21 +1,23 @@
 package playlist;
 
 import java.time.LocalDate;
+
 /**
  * 
- * @author dell
+ * @author Daniela y Lorena
+ * Esta claase se crea para definir la cancion y los atributos que debe llevar la misma, mencionados mas adelante.
  *
  */
 public class Song {
-	
+
 	private String title;
 	private int id;
 	private LocalDate date;
 	private int time;
 	private String gender;
-	private String cover; //Caratula
+	private String cover; // Caratula
 	private String description;
-	
+
 	/**
 	 * 
 	 * @param title
@@ -26,64 +28,82 @@ public class Song {
 	 * @param cover
 	 * @param description
 	 */
+	// Se crea el contructor con todos los parametros que recibe el metodo
 	public Song(String title, int id, LocalDate date, int time, String gender, String cover, String description) {
 		super();
 		this.title = title;
 		this.id = id;
-		this.date =date;
+		this.date = date;
 		this.time = time;
 		this.gender = gender;
 		this.cover = cover;
 		this.description = description;
-	}
-	public String getTitle() {
 		
+		// Getter and Setter
+	}
+
+	public String getTitle() {
+
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public LocalDate getDate() {
 		return date;
 	}
+
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
+
 	public int getTime() {
 		return time;
 	}
+
 	public void setTime(int time) {
 		this.time = time;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public String getCover() {
 		return cover;
 	}
+
 	public void setCover(String cover) {
 		this.cover = cover;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	@Override
+
+	@Override // Sobreescribimos el metodo toString para crear nuestros objetos con los mismos
+				// parametos
 	public String toString() {
 		return "Song [title=" + title + ", id=" + id + ", date=" + date + ", time=" + time + ", gender=" + gender
 				+ ", cover=" + cover + ", description=" + description + "]";
 	}
-	
 
 }

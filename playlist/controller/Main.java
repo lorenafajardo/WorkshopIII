49f65ba202a87fc65af8bcab203c2aee0799f5cf
Ivonne Daniela Montano Gomez <94015994+Daniela97B@users.noreply.github.com>
2022-04-15 15:38:41 -0500

    Update Main.java

--- playlist/controller/Main.java
+++ playlist/controller/Main.java
@@ -1,10 +1,8 @@
 package playlist.controller;
 
-import java.time.LocalDate;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import playlist.Menu;
 import playlist.Operations;
@@ -47,7 +45,7 @@ public class Main {
 			}
 
 		} while (option != 5);
-		hoy.getYear();
+		
 	
 	}
 

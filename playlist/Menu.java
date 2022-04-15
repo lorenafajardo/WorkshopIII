437d4ffb7df1b8586382ae94948b59239a7080e0
Lorena Fajardo Díaz <87463011+lorenafajardo@users.noreply.github.com>
2022-04-15 15:06:29 -0500

    Encabezados opciones 3 y 4
    Se añadió los encabezados para las opciones 3 y 4 del menú.

--- playlist/Menu.java
+++ playlist/Menu.java
@@ -2,7 +2,7 @@ package playlist;
 
 public class Menu {
 
-		public void mostrar() {
+	public void mostrar() {
 		System.out.println("-------------WELCOME---------------");
 		System.out.println("1.Mostrar biblioteca");
 		System.out.println("2.Crear Playlist");
@@ -10,7 +10,18 @@ public class Menu {
 		System.out.println("4.Ordenar canciones");
 		System.out.println("5.EXIT");
 	}
-		
+
+	public void filterShow() {
+		System.out.println(" Como quiere filtrar las canciones:");
+		System.out.println("1. Filtrar canciones del mismo genero");
+		System.out.println("2. Filtrar canciones en el mismo año");
+	}
+
+	public void orderShow() {
+		System.out.println(" Como quiere ordenar las canciones:");
+		System.out.println("1. Ordenar caciones por fecha");
+		System.out.println("2. Ordenar canciones por duración");
+	}
 }
 
 
